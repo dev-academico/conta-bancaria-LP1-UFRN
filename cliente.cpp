5459be39f796c9509b964775785b660ae3ebb92b
Pedro Varela <pedrao.varela@gmail.com>
2025-05-14 13:04:18 -0300

    Comentários nas classes

--- cliente.cpp
+++ cliente.cpp
@@ -1,14 +1,17 @@
 #include "cliente.h"
 
+// Cliente iniciliazando vazio
 Cliente::Cliente() {
   this->nome = "";
   this->cpf = "";
 }
 
+// Cliente iniciliazando com nome e cpf
 Cliente::Cliente(string nome, string cpf) {
   this->nome = nome;
   this->cpf = cpf;
 }
 
+// Retornar dados privados
 string Cliente::GetCPF() { return this->cpf; }
 string Cliente::GetNome() { return this->nome; }
--- contabancaria.cpp
+++ contabancaria.cpp
@@ -2,27 +2,33 @@
 #include <iostream>
 using namespace std;
 
+
+// Inicializar Conta Bancaria
 ContaBancaria::ContaBancaria(int numero, Cliente titular) {
   this->numero = numero;
   this->titular = titular;
   this->saldo = 0.0;
 }
 
+// Inicializar Conta Bancaria com saldo
 ContaBancaria::ContaBancaria(int numero, Cliente titular, double saldo) {
   this->numero = numero;
   this->saldo = saldo;
   this->titular = titular;
 }
 
+// Tranferir x em valor para outra conta
 void ContaBancaria::transferir(double valor, ContaBancaria &destino) {
   if (this->saldo >= valor) {
     this->saldo -= valor;
     destino.depositar(valor);
   } else {
+    // A conta que trasferiu não possui dinheiro suficiente
     cout << "Saldo insuficiente para transferência." << endl;
   }
 }
 
+// Tranferir x em valor para outras 2 contas
 void ContaBancaria::transferir(double valor, ContaBancaria &destino,
                                ContaBancaria &destino2) {
   if (this->saldo >= valor) {
@@ -30,16 +36,20 @@ void ContaBancaria::transferir(double valor, ContaBancaria &destino,
     destino.depositar(valor / 2.0);
     destino2.depositar(valor / 2.0);
   } else {
+    // A conta que trasferiu não possui dinheiro suficiente
     cout << "Saldo insuficiente para transferência." << endl;
   }
 }
 
+// Depositar um valor x
 void ContaBancaria::depositar(double valor) { this->saldo += valor; }
 
+// Sacar um valor x
 void ContaBancaria::sacar(double valor) {
   if (this->saldo >= valor) {
     this->saldo -= valor;
   } else {
+    // A conta não possui saldo suficiente para sacar
     cout << "Saldo insuficiente para saque." << endl;
   }
 }
